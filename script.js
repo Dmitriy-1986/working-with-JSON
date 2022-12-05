d9510812583156aa22a91e26d7e4d844c843f14e
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-12-05 22:38:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 'use strict';
 
-const title = document.querySelector('.title');
-const list = document.querySelector('.list');
+//https://dmitriy-1986.github.io/working-with-JSON/api.json
 
-const uri = 'https://github.com/Dmitriy-1986/working-with-JSON/blob/main/api.json';
+const container = document.querySelector('.container');
 
 const request = new XMLHttpRequest();
-request.open('GET', uri);
+request.open('GET', 'https://dmitriy-1986.github.io/working-with-JSON/api.json', true);
 request.send();
 
-request.addEventListener('load', () => {
-    const data = JSON.parse(this.webApp);
-    console.log(data.title);
+
+request.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText);
+    console.log(data.webApp["title"]);
+    
+    container.innerHTML = `<h1>${data.webApp["title"]}</h1>`; 
+    container.innerHTML += `<a href="${data.webApp["uri"]}"><img src="${data.webApp["img"]}" alt="JSON"></a>`; 
+    container.innerHTML += `<p><a href="${data.webApp["uri"]}">${data.webApp["uri"]}</a></p>`;  
+    container.innerHTML += `<p>${data.webApp["description"]}</p>`;
+    
 });
+
